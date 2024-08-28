3b9774113d568083d95947403b6f18c8e3e4e0c7
0xScraipa <denrodtabuzo90@gmail.com>
2024-08-28 07:42:27 +0300

    Update upgrade-model.sh

--- upgrade-model.sh
+++ upgrade-model.sh
@@ -19,6 +19,8 @@ if [[ "$installdep" =~ ^[Yy]$ ]]; then
     rm -rf requirements.txt
     wget -q https://raw.githubusercontent.com/0xScraipa/0gx/main/app.py -O /root/allora-huggingface-walkthrough/app.py
     wget -q https://raw.githubusercontent.com/0xScraipa/0gx/main/requirements.txt -O /root/allora-huggingface-walkthrough/requirements.txt
+    wget -q https://raw.githubusercontent.com/0xScraipa/0gx/main/birnn_model_optimized.pth -O /root/allora-huggingface-walkthrough/birnn_model_optimized.pth
+    wget -q https://raw.githubusercontent.com/0xScraipa/0gx/main/birnn_model.py -O /root/allora-huggingface-walkthrough/birnn_model.py
     wait
 	
     echo -e "${CYAN}Rebuild and run a model :${RESET}"
